a8676b723c600f7676bc4e72db3c0866ddec74b4
exiler <shaukoay.dev@gmail.com>
2025-09-02 09:19:55 +0800

    Enhanced the sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from multipager import MultiPager
 from pages import Load_and_Clean, EDA, Inference, Modeling, Home
+from typing import Dict, Callable
+
+AppFunc = Callable[[], None]
 
 st.set_page_config(
     page_title="StatsGraph",
@@ -11,21 +14,47 @@ st.set_page_config(
 st.markdown("# StatsGraph")
 
 
-def add_pages(pages: MultiPager) -> MultiPager:
-    pages.add_page("🏠 Home", Home.app)
-    pages.add_page("📂 Load & Clean Data", Load_and_Clean.app)
-    pages.add_page("📊 EDA", EDA.app)
-    pages.add_page("📐 Inference & Hypothesis Testing", Inference.app)
-    pages.add_page("📈 Modeling", Modeling.app)
+def add_pages(pages: MultiPager, page_info: Dict[str, AppFunc]) -> MultiPager:
+    """ 
+    Adds pages to the MultiPager instance.
+    Args:
+        pages (MultiPager): The MultiPager instance to which pages will be added.
+        page_info (Dict[str, AppFunc]): A dictionary mapping page names to their corresponding functions.
+    Returns:
+        MultiPager: The updated MultiPager instance with added pages.
+    """
+
+    for page, func in page_info.items():
+        pages.add_page(page, func)
 
     return pages
 
 if __name__ == "__main__":
     pages = MultiPager()
-    pages = add_pages(pages)
+    # initialise the initial dict
+    page_info: Dict[str, AppFunc] = {
+        "🏠 Home": Home.app,
+        "📂 Load & Clean Data": Load_and_Clean.app,
+        "📊 EDA": EDA.app,
+        "🤖 Modeling": Modeling.app,
+        "🔍 Inference": Inference.app,
+    }
+    
+    pages = add_pages(pages, page_info=page_info)
 
     st.session_state.setdefault("dataframes", None)
     st.session_state.setdefault("state", 0)  # 0: Initial, 1: Data Loaded
 
     pages.run()
+
+    # Conditional pages based on state
+    # if st.session_state.get("state", 0) >= 1:
+    #     conditional_page_info: Dict[str, AppFunc] = {
+    #         "📊 EDA": EDA.app,
+    #         "🤖 Modeling": Modeling.app,
+    #         "🔍 Inference": Inference.app,
+    #     }
+    #     pages = add_pages(pages, page_info=conditional_page_info)
+
+    #     st.rerun()
  
