891aab20b4bf66c9e4ccd98e647845ebbc18eb24
exiler <shaukoay.dev@gmail.com>
2025-09-06 11:28:16 +0800

    solved streamlit select option bug

--- pages/Inference.py
+++ pages/Inference.py
@@ -1,11 +1,28 @@
 import streamlit as st
+import numpy as np
 import scipy
 from utils.common import scaffold_page
+from typing import Callable, Tuple
 
 """
 Numerical: T-Tests, Dunett's test, Kendell's tau test, kurtosis tests, Spearman correlation coefficient
 Categorical: Chi-Squared Test, Fisher's exact test,
 """
+
+HypothesisTest =  Callable[[np.ndarray, np.ndarray, str], Tuple[float, float]]
+
+num_test = {
+    "ttest" : (0,1),
+}
+
+cat_test = {
+    "chi2": (0,1),
+}
+
+available_tests: dict = {
+    "numerical": num_test,
+    "categorical": cat_test,
+}
 
 def app():
     """
@@ -27,8 +44,46 @@ def app():
         st.error("Please ensure that you have uploaded more than 1 datasets.")
         st.stop()
 
+    df_keys = list(st.session_state["dataframes"].keys())
+    df1, df2 = st.session_state["dataframes"][df_keys[0]], st.session_state["dataframes"][df_keys[1]]
+
+    df1_cols = list(df1.columns)
+    df2_cols = list(df2.columns)
+
+    # define 3 columns: set 1, set 2 and test
+    col1, col2, col3 = st.columns(3)
+
+    # have selectbox for users to choose the attributes to test for first 2 sets
+    df1_col = col1.selectbox(
+        label="Dataset 1",
+        options=df1_cols,
+        index=1,
+    )
+
+    df2_col = col2.selectbox(
+        label="Dataset 2",
+        options=df2_cols,
+        index=2,
+    )
+
+    if df1[df1_col].dtype != df2[df2_col]:
+        st.error("Error!! Both columns must have the same data type")
+        st.stop()
     
-
-
+    dtype = df1[df1_col].dtype
+    d_test = available_tests["numerical"] if dtype in ["int64", "float64"] else available_tests["categorical"]
+    
+    test_options = list(d_test.keys())
+    test_select = st.selectbox(
+        label="Statistical Test",
+        options=test_options,
+        index=0,
+    )
+    
+    # TODO: Apply test
+    if test_select:
+        pass
+        # msg = st.toast(f"{test_select} selected")
+        # msg.toast(f"{d_test[test_select]}")
 if __name__ == "__main__":
     app()
